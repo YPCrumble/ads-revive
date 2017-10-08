3975bbb2813567d4a0d2bfc32730d0849a01527e
kkprakasa <kaka.prakasa@gmail.com>
2017-10-08 22:49:14 +0700

    revision: add directory /var/www/html/var/templates_compiled & change it's permission to a+w

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 
 mkdir -p /var/www/html/www/admin/plugins
+mkdir -p /var/www/html/var/templates_compiled
 chown -R www-data:www-data .
 chmod -R a+w /var/www/html/plugins
 chmod -R a+w /var/www/html/www/admin/plugins
+chmod -R a+w /var/www/html/var/templates_compiled
 
 /etc/init.d/php5-fpm start
 nginx -g 'daemon off;'
