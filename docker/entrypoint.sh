d78270f13fca684696db230962bd6476ca4f6e1c
kkprakasa <kaka.prakasa@gmail.com>
2017-10-08 22:36:40 +0700

    revision: remove writeable permission /var/www/html/var/templates_compiled

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,6 @@ mkdir -p /var/www/html/www/admin/plugins
 chown -R www-data:www-data .
 chmod -R a+w /var/www/html/plugins
 chmod -R a+w /var/www/html/www/admin/plugins
-chmod -R a+w /var/www/html/var/templates_compiled
 
 /etc/init.d/php5-fpm start
 nginx -g 'daemon off;'
